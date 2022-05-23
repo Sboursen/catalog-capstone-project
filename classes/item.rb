0c5558694247d5c81554eb8eeef4a08c558668a4
sboursen <dev.sboursen@gmail.com>
2022-05-23 14:13:18 +0100

    Create the Item class
    - Create the Item class in a new file
    - Add custom setters for the one to many associations
    - Add placeholder methods

---
+++ classes/item.rb
@@ -0,0 +1,32 @@
+class Item
+  attr_reader :genre, :label, :author
+  attr_accessor :publish_date
+
+  def initialize(published_date)
+    @id = SecureRandom.uuid
+    @publish_date = published_date
+  end
+
+  def genre=(genre)
+    @genre = genre
+    genre.add_item(self)
+  end
+
+  def label=(label)
+    @label = label
+    label.add_item(self)
+  end
+
+  def author=(author)
+    @author = author
+    author.add_item(self)
+  end
+
+  def move_to_archive
+  end
+
+  private
+
+  def can_be_archived?
+  end
+end
