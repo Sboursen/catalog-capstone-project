a09d8ab1fa6af20e19a87fe25930a127e6967d61
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-05-24 23:48:35 +0100

    Add book table.

--- database/schema.sql
+++ database/schema.sql
@@ -3,3 +3,13 @@ CREATE TABLE label(
   TITLE                   VARCHAR(100) NOT NULL,
   COLOR                   VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE book(
+  ID                      SERIAL PRIMARY KEY,
+  LABEL_ID                INT,
+  PUBLISH_DATE            DATE NOT NULL,
+  ARCHIVED                BOOLEAN NOT NULL,
+  PUBLISHER               VARCHAR(100) NOT NULL,
+  COVER_STATE             VARCHAR(100) NOT NULL,
+  FOREIGN KEY (LABEL_ID) REFERENCES label(ID)
+);
