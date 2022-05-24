ec89c8ae28313a697a404a6f3cbd57d94f64ae7f
sboursen <dev.sboursen@gmail.com>
2022-05-24 11:58:52 +0100

    Fix typo: the author variable to be the instance variable @author

--- classes/item.rb
+++ classes/item.rb
@@ -22,7 +22,7 @@ class Item
 
   def add_author(author)
     @author = author
-    author.add_item(self)
+    @author.add_item(self) unless @author.items_list.include?(self)
   end
 
   def move_to_archive
