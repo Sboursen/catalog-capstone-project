ab7d6b80d69dfeb188d5503e27595634ca44a454
sboursen <dev.sboursen@gmail.com>
2022-05-24 12:18:51 +0100

    Implement can_be_archived? method

--- classes/game.rb
+++ classes/game.rb
@@ -12,5 +12,12 @@ class Game < Item
 
   private
 
-  def can_be_archived?; end
+  def can_be_archived?
+    return super() && !recently_played?
+  end
+
+  private
+
+  def recently_played?
+    (Date.today - @last_played_at).to_f / 365 <= 2
 end
