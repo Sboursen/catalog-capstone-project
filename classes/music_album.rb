df9ba8b8909a71b5004f2c452a9362047352d977
Khalil <khalilnasri92@gmail.com>
2022-05-26 11:20:54 +0100

    add method to_json

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -10,6 +10,15 @@ class MusicAlbum < Item
     @on_spotify = on_spotify
   end
 
+  def self.from_json(json)
+    json = JSON.parse(json) if json.is_a? String
+    MusicAlbum.new(json['name'], json['on_spotify'], json['publish_date'])
+  end
+
+  def to_json(*_args)
+    as_json.to_json
+  end
+
   private
 
   def can_be_archived?
@@ -18,5 +27,13 @@ class MusicAlbum < Item
 
   def on_spotify?
     @on_spotify == true
+  end
+
+  def as_json
+    {
+      name: @name,
+      on_spotify: @on_spotify,
+      publish_date: @publish_date
+    }
   end
 end
