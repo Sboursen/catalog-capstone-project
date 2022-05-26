6c7d8eeba540d36ac8d21361ba9c6e65447917fe
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-05-26 12:11:58 +0100

    Add validate_cover_state.

--- controllers/utils.rb
+++ controllers/utils.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 module Utils
   def self.get_valid_date(name)
     date = ''
@@ -14,12 +16,23 @@ module Utils
 
   def self.validate_boolean
     loop do
-      print 'On spotify? [y/n]'
+      puts 'On spotify? [y/n]'
       on_spotify = gets.chomp
-      next if %w[y n].include?(on_spotify.downcase)
+      next unless %w[y n].include?(on_spotify.downcase)
       return true if on_spotify.downcase == 'y'
 
       return false
+    end
+  end
+
+  def self.validate_cover_state
+    loop do
+      puts 'Cover state: good or bad? [g/b]'
+      cover_state = gets.chomp
+      next unless %w[g b].include?(cover_state.downcase)
+      return 'good' if cover_state.downcase == 'g'
+
+      return 'bad'
     end
   end
 end
